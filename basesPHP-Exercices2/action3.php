88582f8f868fe5c194ee2483d7166775a83fa118
JonCourbon <joncourbon@gmail.com>
2020-10-10 17:04:29 +0200

    Ajout required dans les formulaires de l'exercice Ajout exercice gestion d'éléments non remplis dans le formulaire

--- basesPHP-Exercices2/action3.php
+++ basesPHP-Exercices2/action3.php
@@ -1,28 +1,35 @@
 <?php
-// on a besoin de faire plusieurs fois une action (le calcul de la moyenne) => on peut faire une fonction
-function calculerMoyenne($val1,$val2,$val3){
-  // à coder
-}
+$somme=0;
+$nombrenotes=0;
 
-// Calcul de la moyenne de l'étudiant 1 $moyenne1 à partir des données du formulaire val1_1, ...
-// à coder
+// modifications à faire: prendre en compte que des notes peuvent ne pas avoir été saisies !
 
-// Calcul de la moyenne de l'étudiant 2 $moyenne2 à partir des données du formulaire val1_2, ...
-// à coder
+$somme=$somme+$_GET["note1"];
+$nombrenotes++;
+
+$somme=$somme+$_GET["note2"];
+$nombrenotes++;
+
+$somme=$somme+$_GET["note3"];
+$nombrenotes++;
+
+// modifications à faire: prendre en compte la situation où aucune note n'a été saisie
+$moyenne=$somme/$nombrenotes;
+
  ?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <title>Action 3: moyenne des étudiants</title>
+  <title>Action 3: moyenne</title>
 
 
 
 </head>
 <body>
-  L'étudiant 1 a une moyenne de: <?php echo $moyenne1; ?>.<br/>
-  L'étudiant 2 a une moyenne de: <?php echo $moyenne2; ?>
+La moyenne de l'étudiant est: <?php echo $moyenne; ?>
 </body>
 </html>
--- basesPHP-Exercices2/formulaires.php
+++ basesPHP-Exercices2/formulaires.php
@@ -14,10 +14,10 @@
   <h2>Formulaire 1</h2>
   <form action="action1.php" method="GET">
     <fieldset>
-      <label for="nom">Nom:</label>
-      <input type="text" id="nom" name="nom"><br><br>
-      <label for="prenom">Prénom:</label>
-      <input type="text" id="prenom" name="prenom"><br><br>
+      <label for="nom">Nom*:</label>
+      <input type="text" id="nom" name="nom" required="required"><br/><br/>
+      <label for="prenom">Prénom*:</label>
+      <input type="text" id="prenom" name="prenom" required="required"><br/><br/>
       <input type="submit" value="Afficher nom et prénom">
     </fieldset>
   </form>
@@ -25,10 +25,10 @@
   <h2>Formulaire 2</h2>
   <form action="action2.php" method="GET">
     <fieldset>
-      <label for="val1">Nombre 1:</label>
-      <input type="text" id="val1" name="val1"><br><br>
-      <label for="val2">Nombre 2:</label>
-      <input type="text" id="val2" name="val2"><br><br>
+      <label for="val1">Nombre 1*:</label>
+      <input type="text" id="val1" name="val1" required="required"><br/><br/>
+      <label for="val2">Nombre 2*:</label>
+      <input type="text" id="val2" name="val2" required="required"><br/><br/>
       <input type="submit" value="Calculer et afficher la somme">
     </fieldset>
   </form>
@@ -36,20 +36,35 @@
   <h2>Formulaire 3</h2>
   <form action="action3.php" method="GET">
     <fieldset>
-      <label for="val">Notes étudiant 1:</label>
-      <input type="text" id="val1" name="val1_1" placeholder="val1">
-      <input type="text" id="val2" name="val2_1" placeholder="val2">
-      <input type="text" id="val3" name="val3_1" placeholder="val3"><br><br>
-      <label for="val">Notes étudiant 2:</label>
-      <input type="text" id="val1" name="val1_2" placeholder="val1">
-      <input type="text" id="val2" name="val2_2" placeholder="val2">
-      <input type="text" id="val3" name="val3_2" placeholder="val3"><br><br>
-      <input type="submit" value="Afficher les moyennes">
+      <label for="val1">Note 1:</label>
+      <input type="text" id="val1" name="note1"><br/><br/>
+      <label for="val2">Note 2:</label>
+      <input type="text" id="val2" name="note2"><br/><br/>
+      <label for="val2">Note 3:</label>
+      <input type="text" id="val3" name="note3"><br/><br/>
+      <input type="submit" value="Calculer et afficher la moyenne"><br/></br>
+      (laisser le champ vide si pas de note)
     </fieldset>
   </form>
 
   <h2>Formulaire 4</h2>
   <form action="action4.php" method="GET">
+    <fieldset>
+      <label for="val">Notes étudiant 1*:</label>
+      <input type="text" id="val1" name="val1_1" placeholder="val1" required="required">
+      <input type="text" id="val2" name="val2_1" placeholder="val2" required="required">
+      <input type="text" id="val3" name="val3_1" placeholder="val3" required="required"><br/><br/>
+      <label for="val">Notes étudiant 2*:</label>
+      <input type="text" id="val1" name="val1_2" placeholder="val1" required="required">
+      <input type="text" id="val2" name="val2_2" placeholder="val2" required="required">
+      <input type="text" id="val3" name="val3_2" placeholder="val3" required="required"><br/><br/>
+      <input type="submit" value="Afficher les moyennes">
+    </fieldset>
+  </form>
+
+
+  <h2>Formulaire 5</h2>
+  <form action="action5.php" method="GET">
     <fieldset>
       <label for="vehicules">Marques de vos véhicules:</label><br/>
       <select name="vehicules" multiple="multiple">
